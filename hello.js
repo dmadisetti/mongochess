3dfef20063e16d894513e072e4eacf6e6bdd4727
Dylan Madisetti <dylan.madisetti@gmail.com>
2013-04-10 21:58:04 -0400

    Debugging

--- hello.js
+++ hello.js
@@ -116,7 +116,11 @@ var gameplay = function gameplay (){
       color = color == 'w' ? 'b' : 'w';
       self.after = col * 8 + row;
       console.log(self.enemies[color]);
+      console.log(self.enemies[color].pieces);
+      console.log(self.enemies[color].pieces[0]);
       console.log(self.enemies[color].pieces[0][1]);
+      
+
       for (z=0;z<self.enemies[color].pieces.length;z++){
         self.col = self.enemies[color].pieces[z][1];
         self.row = self.enemies[color].pieces[z][0];
