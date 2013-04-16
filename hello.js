84d9086ed24df93a6af3cfc2a5d861c242daa149
Dylan Madisetti <dylan.madisetti@gmail.com>
2013-04-16 00:48:02 -0400

    Castling

--- hello.js
+++ hello.js
@@ -118,7 +118,7 @@ var gameplay = function gameplay (){
           self.square[args.after[1]][args.after[0]] = self.square[self.row][self.col];
           self.square[self.row][self.col] = self.empty;
           if (self.piece.piece == 'king'){
-            self.enemies[self.piece.color].king = [self.row,self.col];
+            self.enemies[self.piece.color].king = [self.col,self.row];
             console.log('I madde it');
           }
           var king = self.enemies[self.piece.color].king;
