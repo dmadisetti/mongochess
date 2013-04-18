1414af8d694325fba057dc55a1b077dcdc9300e4
Dylan Madisetti <dylan.madisetti@gmail.com>
2013-04-17 23:27:24 -0400

    Break er up

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 var express = require('express')
 ,mustache = require('mustache')
-,gameplay = require('./gameplay.js')
+,gameplay = require('./gameplay')
 ,CryptoJS = require('cryptojs').Crypto
 ,mongo = require('mongodb')
 ,Server = mongo.Server
